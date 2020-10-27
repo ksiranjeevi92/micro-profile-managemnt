f4054affa39faae30b997746ae168f34f8e7c529
SIRANJEEVI <ksiranjeevi92@gmail.com>
2020-10-27 17:14:32 +0530

    Update Django host file

--- deploy/update.sh
+++ deploy/update.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# TODO: Set to URL of git repo.
+PROJECT_GIT_URL='https://github.com/ksiranjeevi92/micro-profile-managemnt.git'
+
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 git pull
